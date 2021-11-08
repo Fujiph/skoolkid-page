081b41ad13ec14d2c169ea7f2f4d1c53781e03aa
Aueboon Fujiph <aueboonruanekamfu@gmail.com>
2021-11-08 19:44:55 +0700

    inital commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app class="font">
+        <Nuxt />
+  </v-app>
+</template>
+
+<style>
+.font{
+  font-family: "Prompt" !important;
+}
+</style>
+
+<script>
+export default {
+
+}
+</script>
